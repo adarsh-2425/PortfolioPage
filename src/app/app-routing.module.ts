cbdd390e86ed6a9beed3419b7e3c5e4326118b53
adarsh-2425 <you@example.comadarsh.lol2425@gmail.com>
2022-07-19 15:35:30 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LandingSectionComponent } from './landing-section/landing-section.component';
+import { SamplesComponent } from './samples/samples.component';
+import { ServicesSectionComponent } from './services-section/services-section.component';
+import { TestimonyComponent } from './testimony/testimony.component';
+
+const routes: Routes = [
+  {
+    path: "",component : LandingSectionComponent,
+    
+      children:[
+        {path:"services-section",component:ServicesSectionComponent},
+        {path:"testimony",component:TestimonyComponent}
+      ]
+  },
+  {path:'about',component:AboutComponent},
+  {path:'samples',component:SamplesComponent},
+   {path:'contact', component: ContactComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LandingSectionComponent } from './landing-section/landing-section.component';
+import { ServicesSectionComponent } from './services-section/services-section.component';
+import { TestimonyComponent } from './testimony/testimony.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { SamplesComponent } from './samples/samples.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LandingSectionComponent,
+    ServicesSectionComponent,
+    TestimonyComponent,
+    FooterComponent,
+    AboutComponent,
+    SamplesComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
